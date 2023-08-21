490c92d1df5d6f07de9bdecba9ba3d9fbfd4fb1b
Riadh Gharbi <riadhgharbi96@gmail.com>
2023-08-21 21:42:32 +0100

    added redux store

---
+++ client/src/store.js
@@ -0,0 +1,14 @@
+import { configureStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleWare = [thunk];
+
+const store = configureStore(rootReducer, initialState, compose(
+    applyMiddleware(...middleWare),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+export default store;
