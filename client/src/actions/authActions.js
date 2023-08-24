2de43bfea42a10461c24e29813bfc996a9c6fe1f
BTrap <riadhgharbi96@gmail.com>
2023-08-24 08:47:39 +0100

    actually pushed auth actions

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -82,3 +82,24 @@ export const logout = () => {
         type: LOGOUT_SUCCESS
     }
 }
+
+
+//Setup config/headers and token
+export const tokenConfig = getState => {
+    //Get token from local storage
+    const token = getState().auth.token;
+
+    //Headers
+    const config = {
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+
+    //If token, add to headers
+    if (token) {
+        config.headers['x-auth-token'] = token;
+    }
+
+    return config;
+}
